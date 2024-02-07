e507d53c387c3c8d6d4443b7df10b1f0f5f68c15
jk <email@jkrause.io>
2024-02-07 14:13:53 +0100

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fees/',
   plugins: [svelte()],
   test: {
     // https://vitest.dev/config/#environment
